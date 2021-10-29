da64e897c667f3316e89fa18826d93e426ad2625
Spencer Heffley <spheffley@yahoo.com>
2021-10-29 14:52:37 -0400

    Added picture for colors article. Added summary for it as well.

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ $( document ).ready(function() {
 
     $('.type-button').on("click", function() {
         $(".modal-title").text("Why Typography Is So Important To Your Website Design");
-        $(".modal-text").text("In this article, Awebco explains the importance of font choices in our websites. It covers various topics such as how to choose the right font for your brand, and how to pair different fonts together nicely.")
+        $(".modal-text").text("In this article, Awebco explains the importance of font choices in our websites. It covers various topics such as how to choose the right font for your brand, and how to pair different fonts together nicely. This is a great article to help to to choose the fonts that are right for you. Font choice is very important, as it will be reflecting your brand as it sits on your website.")
     })
 
     $('.color-button').on("click", function() {
-        $(".modal-title").text("A Comprehensive Guide to Designing Color");
+        $(".modal-title").text("How Important is Color in Website Design?");
+        $(".modal-text").text("In this article, Studio 1 Designs shows why color choice is so important when designing a website. They not only show how to design a color scheme, but also how different colors can invoke different emotions a responses from your visitors. It can be easy to use a random color scheme generator online and call it a day. I've done it many times. But these random color schemes don't have the intentional emotion-invoking aspect that it so important in our websites.")
     })
-
 })
