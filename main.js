9b98ca783bc0d29544b8fa71c137171c1de512d6
Spencer Heffley <spheffley@yahoo.com>
2021-10-28 23:38:11 -0400

    Initial commit for curation project website

---
+++ main.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    $('.button-button').on("click", function() {
+        $(".modal-title").text("A comprehensive guide to designing UX buttons");
+    })
+
+    $('.type-button').on("click", function() {
+        $(".modal-title").text("Why Typography Is So Important To Your Website Design");
+    })
+
+})
