f350ff2756afcd4cd3b3babd6080e102a1aa62fe
Spencer Heffley <spheffley@yahoo.com>
2021-11-19 22:17:46 -0500

    Changed fonts article, and added 3 more

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ $( document ).ready(function() {
     })
 
     $('.type-button').on("click", function() {
-        $(".modal-title").text("Why Typography Is So Important To Your Website Design");
-        $(".modal-text").text("In this article, Awebco explains the importance of font choices in our websites. It covers various topics such as how to choose the right font for your brand, and how to pair different fonts together nicely. This is a great article to help to to choose the fonts that are right for you. Font choice is very important, as it will be reflecting your brand as it sits on your website.")
+        $(".modal-title").text("Choosing Web Fonts: A Beginner’s Guide");
+        $(".modal-text").text("In this article, Google explains the importance of font choices in our websites. It covers various topics such as how to choose the right font for your brand, and how to pair different fonts together nicely. This is a great article to help to to choose the fonts that are right for you. Font choice is very important, as it will be reflecting your brand as it sits on your website.")
     })
 
     $('.color-button').on("click", function() {
@@ -18,5 +18,20 @@ $( document ).ready(function() {
     $('.pwidth-button').on("click", function() {
         $(".modal-title").text("Optimum Web Readability — Max and min width for page text");
         $(".modal-text").text("Ben Shoemate demonstrates the importance of paragraph width to improve readability on your website. He shows many good examples as well as some bad examples to give helpful perspective. I can't think of how many times I've lost my place or started reading the same line again when paragraphs are too wide. This article is sure to help you improve readability on your website.")
+    })
+
+    $('.responsive-button').on("click", function() {
+        $(".modal-title").text("The benefits of using responsive web design");
+        $(".modal-text").text("In this article, Will Fanguy explains the importance of responsive web design and the best practices of making your website responsive for all devices. Responsive web design is more important than ever before and will only continue to get more important. It is an essential part of web design, especially if you intend to reach as many people as possible.")
+    })
+
+    $('.density-button').on("click", function() {
+        $(".modal-title").text("How to Balance Information Density in Web Design");
+        $(".modal-text").text("In this article, Marc Schenker explains how the density of the information on websites affect the users cognitive load as well as navigatibility and readability of the website. He also shows a few examples of websites that handle this well. While too little information can look boring, too much information will turn users away from a site. It is important to find an in-between that works well for your website.")
+    })
+
+    $('.negative-button').on("click", function() {
+        $(".modal-title").text("How to Avoid Negative Emotions in Web Design");
+        $(".modal-text").text("In this article, Rob Bowen explains how negative space in a web page is actually a positive thing, despite how it sounds. Positive space in your website is the actual content of the site. This text, pictures, and videos. This content needs room to breath, however, and that is where negative space comes in. This is a great article explaining how to utilize white space in your website.")
     })
 })
