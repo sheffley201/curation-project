2ded85f007c3c37cc5368140002f1ae32588beff
Spencer Heffley <spheffley@yahoo.com>
2021-12-04 14:36:44 -0500

    Added line breaks into summaries to separate summary from reflection

--- main.js
+++ main.js
@@ -2,41 +2,41 @@ $( document ).ready(function() {
 
     $('.button-button').on("click", function() {
         $(".modal-title").text("A comprehensive guide to designing UX buttons");
-        $(".modal-text").text("In this article, Will Fanguy gives his readers tips and tricks on 5 different types of buttons we see on websites- text, ghost, raised, toggle, and floating action buttons. While users may not notice it, a good button can help them to see it better and be more likely to interact with it. Based on this article, I'm sure we can all make prettier and more eye-catching buttons.")
+        $(".modal-text").html("In this article, Will Fanguy gives his readers tips and tricks on 5 different types of buttons we see on websites- text, ghost, raised, toggle, and floating action buttons. <br><br>While users may not notice it, a good button can help them to see it better and be more likely to interact with it. Based on this article, I'm sure we can all make prettier and more eye-catching buttons.")
     })
 
     $('.type-button').on("click", function() {
         $(".modal-title").text("Choosing Web Fonts: A Beginner’s Guide");
-        $(".modal-text").text("In this article, Google explains the importance of font choices in our websites. It covers various topics such as how to choose the right font for your brand, and how to pair different fonts together nicely. This is a great article to help to to choose the fonts that are right for you. Font choice is very important, as it will be reflecting your brand as it sits on your website.")
+        $(".modal-text").html("In this article, Google explains the importance of font choices in our websites. It covers various topics such as how to choose the right font for your brand, and how to pair different fonts together nicely. <br><br>This is a great article to help to to choose the fonts that are right for you. Font choice is very important, as it will be reflecting your brand as it sits on your website.")
     })
 
     $('.color-button').on("click", function() {
         $(".modal-title").text("How Important is Color in Website Design?");
-        $(".modal-text").text("In this article, Studio 1 Designs shows why color choice is so important when designing a website. They not only show how to design a color scheme, but also how different colors can invoke different emotions a responses from your visitors. It can be easy to use a random color scheme generator online and call it a day. I've done it many times. But these random color schemes don't have the intentional emotion-invoking aspect that it so important in our websites.")
+        $(".modal-text").html("In this article, Studio 1 Designs shows why color choice is so important when designing a website. They not only show how to design a color scheme, but also how different colors can invoke different emotions a responses from your visitors. <br><br>It can be easy to use a random color scheme generator online and call it a day. I've done it many times. But these random color schemes don't have the intentional emotion-invoking aspect that it so important in our websites.")
     })
 
     $('.pwidth-button').on("click", function() {
         $(".modal-title").text("Optimum Web Readability — Max and min width for page text");
-        $(".modal-text").text("Ben Shoemate demonstrates the importance of paragraph width to improve readability on your website. He shows many good examples as well as some bad examples to give helpful perspective. I can't think of how many times I've lost my place or started reading the same line again when paragraphs are too wide. This article is sure to help you improve readability on your website.")
+        $(".modal-text").html("Ben Shoemate demonstrates the importance of paragraph width to improve readability on your website. He shows many good examples as well as some bad examples to give helpful perspective. <br><br>I can't think of how many times I've lost my place or started reading the same line again when paragraphs are too wide. This article is sure to help you improve readability on your website.")
     })
 
     $('.responsive-button').on("click", function() {
         $(".modal-title").text("The benefits of using responsive web design");
-        $(".modal-text").text("In this article, Will Fanguy explains the importance of responsive web design and the best practices of making your website responsive for all devices. Responsive web design is more important than ever before and will only continue to get more important. It is an essential part of web design, especially if you intend to reach as many people as possible.")
+        $(".modal-text").html("In this article, Will Fanguy explains the importance of responsive web design and the best practices of making your website responsive for all devices. <br><br>Responsive web design is more important than ever before and will only continue to get more important. It is an essential part of web design, especially if you intend to reach as many people as possible.")
     })
 
     $('.density-button').on("click", function() {
         $(".modal-title").text("How to Balance Information Density in Web Design");
-        $(".modal-text").text("In this article, Marc Schenker explains how the density of the information on websites affect the users cognitive load as well as navigatibility and readability of the website. He also shows a few examples of websites that handle this well. While too little information can look boring, too much information will turn users away from a site. It is important to find an in-between that works well for your website.")
+        $(".modal-text").html("In this article, Marc Schenker explains how the density of the information on websites affect the users cognitive load as well as navigatibility and readability of the website. He also shows a few examples of websites that handle this well. <br><br>While too little information can look boring, too much information will turn users away from a site. It is important to find an in-between that works well for your website.")
     })
 
     $('.negative-button').on("click", function() {
         $(".modal-title").text("How to Avoid Negative Emotions in Web Design");
-        $(".modal-text").text("In this article, Rob Bowen explains how negative space in a web page is actually a positive thing, despite how it sounds. Positive space in your website is the actual content of the site. This includes text, pictures, and videos. This content needs room to breath, however, and that is where negative space comes in. This is a great article explaining how to utilize white space in your website.")
+        $(".modal-text").html("In this article, Rob Bowen explains how negative space in a web page is actually a positive thing, despite how it sounds. Positive space in your website is the actual content of the site. This includes text, pictures, and videos. <br><br>This content needs room to breath, however, and that is where negative space comes in. This is a great article explaining how to utilize white space in your website.")
     })
 
     $('.nav-button').on("click", function() {
         $(".modal-title").text("How to Create a Great Navigation Bar");
-        $(".modal-text").text("In this article, Tony Ho Tran explains the different ways of handling navigation in websites, some more controversial than others. Tony gives 5 helpful tips for keeping navigation clean and simple. Navigation can be a tricky thing to get right. Too simplistic, and its hard to find what your looking for. Too complicated, you might not know what the right option is when you're trying to get somewhere. This article makes it easy to get navigation right.")
+        $(".modal-text").html("In this article, Tony Ho Tran explains the different ways of handling navigation in websites, some more controversial than others. Tony gives 5 helpful tips for keeping navigation clean and simple. <br><br>Navigation can be a tricky thing to get right. Too simplistic, and its hard to find what your looking for. Too complicated, you might not know what the right option is when you're trying to get somewhere. This article makes it easy to get navigation right.")
     })
 })
