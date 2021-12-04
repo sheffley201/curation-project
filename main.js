86eb1dbba3235194f4cc1d5478eb27264cb9f1e3
Spencer Heffley <spheffley@yahoo.com>
2021-12-04 14:43:49 -0500

    Made the reflection for the buttons article sound less "3rd grade why we should recycle"-y

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
 
     $('.button-button').on("click", function() {
         $(".modal-title").text("A comprehensive guide to designing UX buttons");
-        $(".modal-text").html("In this article, Will Fanguy gives his readers tips and tricks on 5 different types of buttons we see on websites- text, ghost, raised, toggle, and floating action buttons. <br><br>While users may not notice it, a good button can help them to see it better and be more likely to interact with it. Based on this article, I'm sure we can all make prettier and more eye-catching buttons.")
+        $(".modal-text").html("In this article, Will Fanguy gives his readers tips and tricks on 5 different types of buttons we see on websites- text, ghost, raised, toggle, and floating action buttons. <br><br>While users may not notice it, a good button can help them to see it better and be more likely to interact with it. This article will be sure to help you make prettier and more eye-catching buttons.")
     })
 
     $('.type-button').on("click", function() {
